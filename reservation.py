548f9878da1a5da59ecc7fa82608834affaf1797
Nima <nimars1394sh@gmail.com>
2024-08-02 17:12:16 -0500

    Menu and reserved list extract from database added

--- reservation.py
+++ reservation.py
@@ -1,6 +1,8 @@
 from tkinter import *
 import mysql.connector
 
+Id0 = " "
+
 
 def Connection0():
     connection = mysql.connector.connect(
@@ -10,6 +12,22 @@ def Connection0():
         database="reservation_system"  # نام پایگاه داده‌ای که می‌خواهید به آن وصل شوید
     )
     return connection
+
+
+class Person():
+    def __init__(self, username, phone_number, password):
+        self.username = username
+        self.password = password
+        self.phone_number = phone_number
+
+    def ReservedListShow(self):
+        connection = Connection0()
+        cursor = connection.cursor()
+        sql = "SELECT * FROM reserve_info WHERE userID = %s"
+        val = (Id0,)
+        cursor.execute(sql, val)
+        row = cursor.fetchall()
+        print(row)
 
 
 class Login():
@@ -25,11 +43,15 @@ class Login():
         cursor.execute(sql, val)
         row = cursor.fetchone()
         if row:
+            global Id0
+            Id0 = row[0]
             print("ID:", row[0])
             print("Name:", row[1])
             print("phonenumber:", row[2])
             print("password:", row[3])
             if row[3] == self.password:
+                person = Person(self.username, row[3], self.password)
+                person.ReservedListShow()
                 return 1
             else:
                 return 0
@@ -46,27 +68,33 @@ class Signup():
         self.password = password
 
     def insert(self):
-        connection = Connection0()
-        cursor = connection.cursor()
-        insert_query = "INSERT INTO userinfo (Username, phonenumber, password) VALUES (%s, %s, %s)"
-        values = (self.username, self.phonenumber, self.password)
-        cursor.execute(insert_query, values)
-        connection.commit()
-        print(cursor.rowcount, "record inserted.")
-        cursor.close()
-        connection.close()
+        if self.username and self.phonenumber and self.password:
+            connection = Connection0()
+            cursor = connection.cursor()
+            insert_query = "INSERT INTO userinfo (Username, phonenumber, password) VALUES (%s, %s, %s)"
+            values = (self.username, self.phonenumber, self.password)
+            cursor.execute(insert_query, values)
+            connection.commit()
+            print(cursor.rowcount, "record inserted.")
+            cursor.close()
+            connection.close()
+            return 1
+        else:
+            return 0
 
 
 def LoginCheck(username, password):
     login = Login(username, password)
     if login.checkinfo() == 1:
-        wellcom_label.config(text="log in successfull", foreground="green")
+        wellcom_label.config(
+            text="log in successfull \n WellCome", foreground="green")
         signup_show_button.pack_forget()
         username_label.pack_forget()
         username_entry.pack_forget()
         password_label.pack_forget()
         password_entry.pack_forget()
         login_button.pack_forget()
+        Window.config(menu=menubar)
     elif login.checkinfo() == 0:
         wellcom_label.config(text="log in failed", foreground="red")
     else:
@@ -77,16 +105,18 @@ def LoginCheck(username, password):
 def SignupCheck(username, phone_number, password):
     # arguman ha b database ezafe meshan
     signup = Signup(username, phone_number, password)
-    signup.insert()
-    wellcom_label.config(text="sign up seccessfull please log in ")
-    username_label.pack_forget()
-    username_entry.pack_forget()
-    username_entry.pack_forget()
-    password_label.pack_forget()
-    password_entry.pack_forget()
-    phonenumber_label.pack_forget()
-    phonenumber_entry.pack_forget()
-    signup_button.pack_forget()
+    if signup.insert() == 1:
+        wellcom_label.config(text="sign up seccessfull please log in ")
+        username_label.pack_forget()
+        username_entry.pack_forget()
+        username_entry.pack_forget()
+        password_label.pack_forget()
+        password_entry.pack_forget()
+        phonenumber_label.pack_forget()
+        phonenumber_entry.pack_forget()
+        signup_button.pack_forget()
+    else:
+        wellcom_label.config(text="Fill the information correctly")
 
 
 def loginButtonCommand():
@@ -132,9 +162,10 @@ Window.resizable(width=False, height=False)
 wellcom_label = Label(
     Window, text="Wellcome to reservation System", foreground="blue", font=(20))
 wellcom_label.pack()
-login_show_button = Button(Window, text="Log in page", command=loginpage)
+login_show_button = Button(Window, text="Log in page",
+                           command=loginpage, anchor="e")
 signup_show_button = Button(
-    Window, text="Creat an account", command=signuppage)
+    Window, text="Creat an account", command=signuppage, anchor="center")
 login_show_button.pack()
 signup_show_button.pack()
 username_label = Label(Window, text="Username:")
@@ -145,4 +176,17 @@ phonenumber_label = Label(Window, text="Phone Number")
 phonenumber_entry = Entry(Window)
 login_button = Button(Window, text="Login", command=loginButtonCommand)
 signup_button = Button(Window, text="sign Up", command=signupButtonCommand)
+menubar = Menu(Window)
+reservemenu = Menu(menubar, tearoff=0)
+reservemenu.add_command(label="1.Dermatologist")
+reservemenu.add_command(label="2.Cardiologist")
+reservemenu.add_command(label="3.General practitioner")
+reservemenu.add_command(label="4.Gynecologist")
+reservemenu.add_command(label="5.Neurologist")
+menubar.add_cascade(label="Reserve", menu=reservemenu)
+menubar.add_command(label="My Reservs")
+menubar.add_command(label="Setting")
+menubar.add_command(label="Support")
+menubar.add_command(label="Help")
+menubar.add_command(label="quit", command=Window.quit)
 Window.mainloop()
